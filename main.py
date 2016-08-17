2a03e1884f8678546c1e2fbca7007dc9c81c86c8
funemy <yanzeli@hustuniqe.com>
2016-08-17 09:26:35 +0800

    csv supported

--- main.py
+++ main.py
@@ -26,27 +26,31 @@ sheets = settings['sheets']
 index = settings['sheets_index']
 col_names = settings['col_names']
 date_col = settings['date_col']
+date_col_index = settings['date_col_index']
 date = settings['date']
 time_interval = settings['time_interval']
 
-def process_csv(file, path):
-    pass
-
-def process_xls(file, path):
+def process_data(file, path):
     file_path = os.path.join(path, file)
-    print("正在处理%s" % file_path)
-    tables = utils.get_xls_tables(file_path, sheets, index)
-    cols = utils.get_table_column(tables, date_col, col_names)
-    for task in [utils.max_column_data,
-                 utils.fix_time_interval,
-                 utils.check_missing_data,
-                 utils.standardize_col_data]:
-        cols = task(cols, date_col)
+    if utils.check_ext(file, ['.xls', '.xlsx']):
+        print("正在处理%s" % file_path)
+        tables = utils.get_xls_tables(file_path, sheets, index)
+        cols = utils.get_table_column(tables, col_names, date_col_index, date_col)
+    elif utils.check_ext(file, ['.csv', '.CSV']):
+        print("正在处理%s" % file_path)
+        csv_list = utils.get_csv_list(file_path)
+        cols = utils.get_csv_column(csv_list, col_names, date_col_index, date_col)
+    else:
+        return None
+    cols = utils.max_column_data(cols)
+    cols = utils.fix_time_interval(cols, time_interval)
+    cols = utils.check_missing_data(cols)
+    cols = utils.standardize_col_data(cols)
     return cols
+
 
 def output(cols, path, filename):
     utils.cols_to_csv(cols, path, filename)
-    pass
 
 def batch_standardize_data(project_path, data_path):
     utils.check_os()
@@ -55,14 +59,12 @@ def batch_standardize_data(project_path, data_path):
     all_file_list = utils.read_recursive_dir(path)
     for file_dict in all_file_list:
         for file in file_dict['file_list']:
-            if utils.check_ext(file, ['.xls', '.xlsx']):
-                cols = process_xls(file, file_dict['path'])
-            # elif utils.check_ext(file, ['.csv', '.CSV']):
-            #     cols = process_csv(file, file_dict['path'])
+            cols = process_data(file, file_dict['path'])
+            if cols:
+                cols = utils.select_data_period(cols, date, time_interval)
+                output(cols, file_dict['path'], file)
             else:
                 continue
-            utils.select_data_period(cols, date_col, date, time_interval)
-            output(cols, file_dict['path'], file)
 
 if __name__ == '__main__':
     batch_standardize_data(project_path, data_path)
